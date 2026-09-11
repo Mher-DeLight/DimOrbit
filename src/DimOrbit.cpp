5a60a6c22b454ae51cb856acf30dc3bd91e27ad5
Mher DeLight <mahirzamil2012@gmail.com>
2026-09-11 13:30:17 +0300

    initial commit

---
+++ src/DimOrbit.cpp
@@ -0,0 +1,8 @@
+#include "../include/DimOrbit/DimOrbit.h"
+#include <iostream>
+
+namespace DimOrbit {
+void sayHello() {
+    std::cout << "Hello, world!" << std::endl;
+}
+} // namespace DimOrbit
---
+++ tests/main.cpp
@@ -0,0 +1,5 @@
+#include <DimOrbit/DimOrbit.h>
+
+int main(int, char**) {
+    DimOrbit::sayHello();
+}
