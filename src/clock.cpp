3039a1b54edd15109172fa25ef7581a691160f7a
Mher DeLight <mahirzamil2012@gmail.com>
2026-09-17 21:18:44 +0300

    fix delta bug that makes run 16x the realtime speed

--- src/clock.cpp
+++ src/clock.cpp
@@ -129,7 +129,8 @@ Clock::Clock(CelestialSystem& startSystem) {
 void Clock::tick(float delta) {
     int ticks = time.tick(delta * speedScale);
     for (int i = 0; i < ticks; i++) {
-        getSystem().tick(delta);
+        // we tick by 1ms because ticks returns approximately the amount of milliseconds in delta
+        getSystem().tick(0.001f);
     }
 }
 
